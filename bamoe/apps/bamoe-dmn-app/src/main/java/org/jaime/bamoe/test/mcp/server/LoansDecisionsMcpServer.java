f8975d03574de12c9b5243f56f3c31b0d82b8937
Jaime Izuzquiza Suárez-Inclán <jizuzquiza@hotmail.es>
2025-11-25 12:46:14 +0100

    Refine prompt info

--- bamoe/apps/bamoe-dmn-app/src/main/java/org/jaime/bamoe/test/mcp/server/LoansDecisionsMcpServer.java
+++ bamoe/apps/bamoe-dmn-app/src/main/java/org/jaime/bamoe/test/mcp/server/LoansDecisionsMcpServer.java
@@ -6,19 +6,11 @@ import jakarta.inject.Singleton;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.kie.dmn.feel.lang.types.impl.ComparablePeriod;
-import org.kie.kogito.Application;
-import org.kie.kogito.dmn.rest.DMNFEELComparablePeriodSerializer;
 import org.kie.kogito.incubation.application.AppRoot;
 import org.kie.kogito.incubation.common.DataContext;
 import org.kie.kogito.incubation.common.MapDataContext;
 import org.kie.kogito.incubation.decisions.DecisionIds;
 import org.kie.kogito.incubation.decisions.services.DecisionService;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import io.quarkiverse.mcp.server.McpLog;
 import io.quarkiverse.mcp.server.Prompt;
@@ -28,7 +20,6 @@ import io.quarkiverse.mcp.server.TextContent;
 import io.quarkiverse.mcp.server.Tool;
 import io.quarkiverse.mcp.server.ToolArg;
 import io.smallrye.common.annotation.RunOnVirtualThread;
-
 
 import io.quarkus.logging.Log;
 
@@ -41,7 +32,9 @@ public class LoansDecisionsMcpServer {
     @Inject
     DecisionService svc;
 
-    @Tool(name = "performLoanApplicationEligibilityChecks", description = "Evaluates the eligibility of an applicant requesting a loan")
+    @Tool(
+        name = "evaluate_loan_application_eligibility_checks",
+        description = "Evaluates the eligibility of an applicant requesting a loan")
     @RunOnVirtualThread
     public DataContext performLoanApplicationEligibilityChecks(
             @ToolArg(description = "Loan duration (in months)") Integer loanDuration,
@@ -67,8 +60,10 @@ public class LoansDecisionsMcpServer {
         return svc.evaluate(id, MapDataContext.of(inputs)).data();
     }
 
-    @Prompt(description = "Asks the Loans Agent to evaluate a Loan Application Eligibility given some required data") 
-    PromptMessage foo(
+    @Prompt(
+        name = "analyze_loan_application_eligibility",
+        description = "Asks the Loans Agent to analyze a Loan Application Eligibility given applicant's data") 
+    PromptMessage analyzeLoanApplicantEligibility(
             @PromptArg(description = "Loan duration (in months)") String loanDuration,
             @PromptArg(description = "Loan amount (in Euros)") String loanAmount,
             @PromptArg(description = "Applicant monthly incomes (in Euros)") String applicantMonthlyIncomes,
