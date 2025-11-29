550671f8dc62aee4bcfb8929a6fc70b4346f50de
Jaime Izuzquiza Suárez-Inclán <jizuzquiza@hotmail.es>
2025-11-29 20:52:29 +0100

    Add PGVector and use separated postgresql for AI

---
+++ sql/postgres-ai-server/init.sql
@@ -0,0 +1,38 @@
+CREATE ROLE "langflow-user" WITH
+    LOGIN
+    SUPERUSER
+    INHERIT
+    CREATEDB
+    CREATEROLE
+    NOREPLICATION
+    PASSWORD 'langflow-pass';
+
+CREATE ROLE "mcpgw-user" WITH
+    LOGIN
+    SUPERUSER
+    INHERIT
+    CREATEDB
+    CREATEROLE
+    NOREPLICATION
+    PASSWORD 'mcpgw-pass';
+
+CREATE DATABASE langflow
+    WITH
+    OWNER = "kogito-user"
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+CREATE DATABASE mcpgw
+    WITH
+    OWNER = "mcpgw-user"
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+GRANT ALL PRIVILEGES ON DATABASE langflow TO "langflow-user";
+GRANT ALL PRIVILEGES ON DATABASE mcpgw TO "mcpgw-user";
