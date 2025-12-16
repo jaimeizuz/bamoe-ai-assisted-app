9a07c4b86cbd66e722ccb904faf01529df0f4ace
jaime <jaime@localhost.localdomain>
2025-12-16 13:30:49 +0100

    Several changes

--- sql/postgres-ai-server/init.sql
+++ sql/postgres-ai-server/init.sql
@@ -7,6 +7,15 @@ CREATE ROLE "langflow-user" WITH
     NOREPLICATION
     PASSWORD 'langflow-pass';
 
+CREATE ROLE "langfuse-user" WITH
+    LOGIN
+    SUPERUSER
+    INHERIT
+    CREATEDB
+    CREATEROLE
+    NOREPLICATION
+    PASSWORD 'langfuse-pass';
+
 CREATE ROLE "mcpgw-user" WITH
     LOGIN
     SUPERUSER
@@ -25,6 +34,15 @@ CREATE DATABASE langflow
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
+CREATE DATABASE langfuse
+    WITH
+    OWNER = "langfuse-user"
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
 CREATE DATABASE mcpgw
     WITH
     OWNER = "mcpgw-user"
@@ -35,6 +53,8 @@ CREATE DATABASE mcpgw
     CONNECTION LIMIT = -1;
 
 GRANT ALL PRIVILEGES ON DATABASE langflow TO "langflow-user";
+GRANT ALL PRIVILEGES ON DATABASE langfuse TO "langfuse-user";
 GRANT ALL PRIVILEGES ON DATABASE mcpgw TO "mcpgw-user";
 GRANT ALL PRIVILEGES ON DATABASE langflow TO "postgres";
+GRANT ALL PRIVILEGES ON DATABASE langfuse TO "postgres";
 GRANT ALL PRIVILEGES ON DATABASE mcpgw TO "postgres";
