2bb0209c2ce524d4dab61ae6bdc9660db54fc9be
jaimeizuz <jizuzquiza@hotmail.es>
2026-03-26 17:24:17 +0100

    New database for embeddings

--- sql/postgres-ai-server/init.sql
+++ sql/postgres-ai-server/init.sql
@@ -70,6 +70,15 @@ CREATE DATABASE mcpgw
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
+CREATE DATABASE embeddings
+    WITH
+    OWNER = "langflow-user"
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
 GRANT ALL PRIVILEGES ON DATABASE langflow TO "langflow-user";
 GRANT ALL PRIVILEGES ON DATABASE openwebui TO "openwebui-user";
 GRANT ALL PRIVILEGES ON DATABASE langfuse TO "langfuse-user";
@@ -78,3 +87,5 @@ GRANT ALL PRIVILEGES ON DATABASE langflow TO "postgres";
 GRANT ALL PRIVILEGES ON DATABASE openwebui TO "postgres";
 GRANT ALL PRIVILEGES ON DATABASE langfuse TO "postgres";
 GRANT ALL PRIVILEGES ON DATABASE mcpgw TO "postgres";
+GRANT ALL PRIVILEGES ON DATABASE embeddings TO "langflow-user";
+GRANT ALL PRIVILEGES ON DATABASE embeddings TO "postgres";
